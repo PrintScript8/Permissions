1ba351b5b5ecb52a4db4df6a48d4827f8159f9d3
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-10-07 23:21:57 -0300

    Added Tests and fix Fix to the aplication to properly set variables

--- src/main/kotlin/austral/ingsis/permissions/PermissionsApplication.kt
+++ src/main/kotlin/austral/ingsis/permissions/PermissionsApplication.kt
@@ -8,6 +8,7 @@ import org.springframework.boot.runApplication
 class PermissionsApplication
 
 fun main(args: Array<String>) {
-    Dotenv.load()
+    val dotenv = Dotenv.load()
+    dotenv.entries().forEach { entry -> System.setProperty(entry.key, entry.value) }
     runApplication<PermissionsApplication>(args.toString())
 }
